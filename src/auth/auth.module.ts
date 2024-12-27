5323900ac80a5479ca3cefab05289a3d62994b4f
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-12-27 18:08:26 -0300

    feat(auth): implement jwt validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [UserModule, JwtModule.register({
@@ -12,6 +13,6 @@ import { JwtModule } from '@nestjs/jwt';
   }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule { }
